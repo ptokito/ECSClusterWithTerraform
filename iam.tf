6ffe1bb9d4c674ddadd3d02e6d5ca883f3c8b0ed
aaazzz <aaazzz@users.noreply.github.com>
2020-10-18 22:16:17 +0700

    Update iam.tf
    `role` in `aws_iam_role_policy_attachment` should refer the `name` of `aws_iam_role.ecs_agent`

--- iam.tf
+++ iam.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "ecs_agent" {
 
 
 resource "aws_iam_role_policy_attachment" "ecs_agent" {
-  role       = "aws_iam_role.ecs_agent"
+  role       = aws_iam_role.ecs_agent.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 resource "aws_iam_instance_profile" "ecs_agent" {
